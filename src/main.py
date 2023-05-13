8d7748aeaa03ab568f269a3b5be71251f4bd1e12
Xan <xandrabest@gmail.com>
2023-05-13 15:42:25 +0100

    refactoring for best practices

--- src/main.py
+++ src/main.py
@@ -1,33 +1,4 @@
-import json
-
 from service import todoist_service
 
-
-
-habit_task_completions = {}
-
-
-#deserialize from json file
-with open('user_data/habit_tasks_completions.json', 'r') as file:
-    habit_task_completions = json.load(file)
-for habit_id in habit_task_completions:
-    for task_id in habit_task_completions[habit_id]:
-        habit_task_completions[habit_id][task_id] = set(habit_task_completions[habit_id][task_id])
-
-for habit_id in habit_task_completions:
-    for task_id in habit_task_completions[habit_id]:
-        habit_task_completions[habit_id][task_id].update(todoist_service.getTaskCompletions(task_id))
-
-
-#serialize to json
-for habit_id in habit_task_completions:
-    for task_id in habit_task_completions[habit_id]:
-        habit_task_completions[habit_id][task_id] = list(habit_task_completions[habit_id][task_id])
-        habit_task_completions[habit_id][task_id].sort(reverse=True)
-jsondata = json.dumps(habit_task_completions, indent=4)
-
-#save to file
-with open('user_data/habit_tasks_completions.json', 'w') as outfile:
-    outfile.write(jsondata)
-
+todoist_service.updateHabitTaskCompletions()
 
--- src/service/todoist_service.py
+++ src/service/todoist_service.py
@@ -1,4 +1,31 @@
+import json
+
 from src.client import todoist_client
+from src.constants.data_constants import *
+
+def updateHabitTaskCompletions():
+    habit_task_completions = readHabitTaskCompletions()
+
+    # update
+    for habit_id in habit_task_completions:
+        for task_id in habit_task_completions[habit_id]:
+            habit_task_completions[habit_id][task_id].update(getTaskCompletions(task_id))
+
+    writeHabitTaskCompletions(habit_task_completions)
+
+
+def readHabitTaskCompletions():
+    # deserialize from json file
+    with open(FILE_PATH_HABIT_TASK_COMPLETIONS, CODE_READ) as file:
+        habit_task_completions = json.load(file)
+
+    # change completion list to set
+    for habit_id in habit_task_completions:
+        for task_id in habit_task_completions[habit_id]:
+            habit_task_completions[habit_id][task_id] = set(habit_task_completions[habit_id][task_id])
+
+    return habit_task_completions
+
 
 def getTaskCompletions(object_id):
     response = todoist_client.getTaskCompletions(object_id)
@@ -9,11 +36,17 @@ def getTaskCompletions(object_id):
     for event in response.json()['events']:
         event_timestamps.add(event['event_date'])
 
-    print(event_timestamps)
-    print(type(event_timestamps))
-
     return event_timestamps
 
 
+def writeHabitTaskCompletions(habit_task_completions):
+    # serialize to json
+    for habit_id in habit_task_completions:
+        for task_id in habit_task_completions[habit_id]:
+            habit_task_completions[habit_id][task_id] = list(habit_task_completions[habit_id][task_id])
+            habit_task_completions[habit_id][task_id].sort(reverse=True)
+    jsondata = json.dumps(habit_task_completions, indent=4)
 
-
+    # save to file
+    with open(FILE_PATH_HABIT_TASK_COMPLETIONS, CODE_WRITE) as outfile:
+        outfile.write(jsondata)
